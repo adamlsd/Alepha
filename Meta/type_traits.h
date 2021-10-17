d870367f7632b3eb703b0296db55fa6040bdac2b
ADAM David Alan Martin <adam@recursive.engineer>
2021-10-16 23:43:44 -0400

    I added the `type_traits` header.

---
+++ Meta/type_traits.h
@@ -0,0 +1,21 @@
+static_assert( __cplusplus > 201700, "C++17 Required" );
+
+#pragma once
+
+#include <is_deque.h>
+#include <is_forward_list.h>
+#include <is_functional.h>
+#include <is_list.h>
+#include <is_map.h>
+#include <is_optional.h>
+#include <is_pair.h>
+#include <is_product_type.h>
+#include <is_sequence.h>
+#include <is_set.h>
+#include <is_streamable.h>
+#include <is_string.h>
+#include <is_tuple.h>
+#include <is_vector.h>
+#include <pair_decay.h>
+#include <product_type_decay.h>
+#include <tuple_decay.h>
