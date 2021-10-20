744e02fb01214c84b9487f3c135844fdc1b011e2
ADAM David Alan Martin <adam@recursive.engineer>
2021-10-19 22:28:45 -0400

    Concept-like overloading of functions support.
    When using `enable_if` clauses in templates, we need an NTTP
    in the mix to let us get good overloading support.  The boolean
    true/false trick of the `enable_if` second parameter actually
    doesn't work for many situations.

---
+++ Meta/overload.h
@@ -0,0 +1,32 @@
+static_assert( __cplusplus > 201700, "C++17 Required" );
+
+#pragma once
+
+#include <Alepha/Alepha.h>
+
+namespace Alepha::Hydrogen::Meta
+{
+	inline namespace exports { inline namespace template_overload {} }
+
+	namespace detail::template_overload
+	{
+		inline namespace exports {}
+
+		template< std::size_t >
+		struct holder
+		{
+			struct overload_t;
+		};
+
+		namespace exports
+		{
+			template< std::size_t i >
+			using overload= typename holder< i >::overload_t *;
+		}
+	}
+
+	namespace exports::template_overload
+	{
+		using namespace detail::template_overload::exports;
+	}
+}
