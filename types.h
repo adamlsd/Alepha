77532ca8eea46725ed216a86f5ac0ddc07646e9b
ADAM David Alan Martin <adam@recursive.engineer>
2021-10-25 01:58:54 -0400

    These types might help `main` declarations cleaner.

---
+++ types.h
@@ -0,0 +1,24 @@
+static_assert( __cplusplus > 201700, "C++17 Required" );
+
+#pragma once
+
+#include <Alepha/Alepha.h>
+
+namespace Alepha::Hydrogen
+{
+	inline namespace exports { inline namespace types {} }
+
+	namespace detail::types
+	{
+		inline namespace exports
+		{
+			using argvec_t= const char *const [];
+			using argcnt_t= int;
+		}
+	}
+
+	namespace exports::types
+	{
+		using namespace detail::types::exports;
+	}
+}
