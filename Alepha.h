0a1ecce9eca7528e1e332f1f9939a35f80d7ad80
ADAM David Alan Martin <adam@recursive.engineer>
2022-10-17 22:50:33 -0400

    Permit user-specification of the real namespace of boost.
    This way `Alepha` can be used in a binary-only library project which
    uses boost or a codebase which mangles boost.

--- Alepha.h
+++ Alepha.h
@@ -2,7 +2,20 @@ static_assert( __cplusplus > 201700, "C++17 Required" );
 
 #pragma once
 
+#ifndef ALEPHA_OVERRIDE_BOOST_NAMESPACE
+namespace boost {}
+#else
+namespace ALEPHA_OVERRIDE_BOOST_NAMESPACE {}
+#endif
+
 namespace Alepha
 {
-	inline namespace Hydrogen {}
+	inline namespace Hydrogen
+	{
+		#ifndef ALEPHA_OVERRIDE_BOOST_NAMESPACE
+		namespace boost_ns= ::boost;
+		#else
+		namespace boost_ns= ALEPHA_OVERRIDE_BOOST_NAMESPACE;
+		#endif
+	}
 }
